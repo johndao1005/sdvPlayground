7f9944d91604ba6eecb16ee5082b411d5da54b61
johndao1005 <john.d1729@gmail.com>
2021-04-12 21:22:12 +1200

    first code dojo

--- functions/24h-convert.js
+++ functions/24h-convert.js
@@ -1,18 +1,25 @@
 // input: "13:10"
 // output: "1:30pm"
-let input = '2:30'
+let input = '18:30'
+    //ANCHOR need to add some restrain function
 
-function timeconvert(input) {
-    let [hours, minutes] = input.split(':')
-        //convert time
-    if (hours > 12) {
-        // checking if it is am or pm
-        hours = hours - 12
-            //also changing it into pm time
-        return hours + minutes + 'pm'
-    } else {
-        return input + 'am'
-    }
+//ANCHOR original function
+// function timeconvert(input) {
+//     let [hours, minutes] = input.split(':')
+//convert time
+//     if (hours > 12) {
+// checking if it is am or pm
+//         hours = hours - 12
+//also changing it into pm time
+//         return hours + minutes + 'pm'
+//     } else {
+//         return input + 'am'
+//     }
+// }
+//ANCHOR factoried function
+const timeconvert = (input) => {
+    [hours, minutes] = input.split(':')
+    return hours > 12 ? (hours - 12) + ':' + minutes + 'pm' : input + 'am'
 }
 console.log(timeconvert(input))
     //can choose where to split
