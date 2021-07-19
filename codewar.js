5b026e361ee14e2fd8c226d0b7a522e701aa6eb0
johndao1005 <john.d1729@gmail.com>
2021-07-20 09:34:04 +1200

    learn from codewar

--- codewar.js
+++ codewar.js
@@ -63,4 +63,119 @@ var uniqueInOrder = function(iterable) {
     }
     return result
 }
-console.log(uniqueInOrder('aaabbbaaabcbcbcbcbbbbbb'))
+
+// checking if the word is palindrome
+function palindrome(str) {
+    let re = /[a-zA-Z||0-9]/g
+    let string = (str.toLowerCase()).match(re)
+    return string.join('') == string.reverse().join('');
+}
+
+// roman number
+function convertToRoman(num) {
+    const roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    let result = ''
+    for (let i of Object.keys(roman)) {
+        let c = Math.floor(num / roman[i]);
+        num -= c * roman[i];
+        result += i.repeat(c)
+    }
+    return result
+}
+
+// Correct answers
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+function telephoneCheck(str) {
+    let re = /^(1\s|1)?(\(\d{3}\)|\d{3})(\s|-)?\d{3}(\s|-)?\d{4}$/g
+    return re.test(str)
+}
+
+
+function rot13(str) {
+    let result = []
+    let arr = [...str]
+    let re = /[\.\s\?\!]/
+    arr.forEach(a => !re.test(a) ? result.push(String.fromCharCode(a.charCodeAt() <= 77 ? a.charCodeAt() + 13 : a.charCodeAt() - 13)) : result.push(a))
+    return result.join("");
+}
+
+const currencyUnit = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.1,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
+    "ONE HUNDRED": 100
+}
+
+function checkCashRegister(price, cash, cid) {
+    let changeSum = cash - price;
+    let changeSumCheck = changeSum
+    let change = [];
+    let status = '';
+
+    let cidSum = 0;
+    let filteredCid = cid.filter(elem => elem[1] !== 0).reverse();
+
+    filteredCid.forEach(elem => {
+        let curr = elem[0];
+        let currSum = elem[1];
+        cidSum += currSum;
+        let amount = 0;
+        while (changeSum >= currencyUnit[curr] && currSum > 0) {
+            amount += currencyUnit[curr];
+            changeSum -= currencyUnit[curr];
+            currSum -= currencyUnit[curr];
+        }
+        if (amount !== 0) {
+            change.push([curr, amount]);
+        }
+    })
+    if (changeSum > 0) {
+        status = 'INSUFFICIENT_FUNDS'
+        change = []
+    } else if (changeSum == 0 && changeSumCheck == cidSum) {
+        status = "CLOSED"
+        change = cid;
+    } else {
+        status = "OPEN"
+        change
+        cid
+    }
+    return { 'status': status, 'change': change }
+}
+
+console.log(checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+]))
